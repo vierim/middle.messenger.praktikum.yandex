52a71a7ace74257e6cc21fdf04f6e6fdf1fb8da7
Vladimir Ryabtsev <86350622+vierim@users.noreply.github.com>
2024-05-02 09:49:36 +0300

    Add error page template

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Handlebars from 'handlebars';
 
 import basicLayout from './layout/main';
-import loginPage from './pages/login';
-import form from './modules/login-form';
+import { loginPage, registrationPage, feedPage, errorPage } from './pages';
+
+import loginForm from './modules/login-form';
+import registrationForm from './modules/registration-form';
+import { searchBar, contactsList } from './modules';
 
 import { button, input } from './components';
 
@@ -12,16 +15,34 @@ const root = document.querySelector('#app');
 
 Handlebars.registerPartial('input', input);
 Handlebars.registerPartial('button', button);
-Handlebars.registerPartial('form', form);
+Handlebars.registerPartial('login-form', loginForm);
+Handlebars.registerPartial('registration-form', registrationForm);
+Handlebars.registerPartial('search-bar', searchBar);
+Handlebars.registerPartial('contacts-list', contactsList);
 
 const basicLayoutTmpl = Handlebars.compile(basicLayout);
 
 const loginPageDelegate = Handlebars.compile(loginPage);
 const loginPageContent = loginPageDelegate({
   title: 'Вход',
-  form: form,
+  form: loginForm,
+});
+
+const registrationPageDelegate = Handlebars.compile(registrationPage);
+const registrationPageContent = registrationPageDelegate({
+  title: 'Регистрация',
+  form: registrationForm,
+});
+
+const feedPageDelegate = Handlebars.compile(feedPage);
+const feedPageContent = feedPageDelegate();
+
+const errorPageDelegate = Handlebars.compile(errorPage);
+const errorPageContent = errorPageDelegate({
+  code: 404,
+  text: 'Не туда попали'
 });
 
 root.innerHTML = basicLayoutTmpl({
-  content: loginPageContent,
+  content: errorPageContent,
 });
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import loginPage from './login';
+import registrationPage from './registration';
+import feedPage from './feed';
+import errorPage from './error';
+
+export { loginPage, registrationPage, feedPage, errorPage };
