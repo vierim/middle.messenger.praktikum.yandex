60f0ee159fa7268c24a0a8d158b84d2d71ad1c6c
Vladimir Ryabtsev <86350622+vierim@users.noreply.github.com>
2024-05-05 08:55:28 +0300

    Add netlify.toml config

--- server/index.js
+++ server/index.js
@@ -1,18 +1,27 @@
 import express from 'express';
+const serverless = require("serverless-http");
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
 
-const { PORT = 3000 } = process.env;
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const pathName = resolve(__dirname, '../dist/index.html');
-
-app.use(express.static('./dist'));
-
-app.get('/*', (req, res) => {
-  res.sendFile(pathName);
+const router = express.Router();
+router.get("/", (_, res) => {
+  res.send("hello world!")
 });
+app.use(`/.netlify/functions/app`, router);
+module.exports = app;
+module.exports.handler = serverless(app);
 
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+// const { PORT = 3000 } = process.env;
+
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+// const pathName = resolve(__dirname, '../dist/index.html');
+
+// app.use(express.static('./dist'));
+
+// app.get('/*', (req, res) => {
+//   res.sendFile(pathName);
+// });
+
+// app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
