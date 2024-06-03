46f3613562002ad278bec0653a4d25556b412609
Vladimir Ryabtsev <86350622+vierim@users.noreply.github.com>
2024-06-03 08:39:59 +0300

    fix: Correct type for Component event listeners list

--- src/core/component/types.ts
+++ src/core/component/types.ts
@@ -3,7 +3,7 @@ import Component from "./component";
 export type Children = Record<string, Component>;
 
 export type Props = Record<string, unknown> & {
-  events?: Record<string, () => void>;
+  events?: Record<string, (event: Event) => void>;
 };
 
 export type Lists = Record<string, Array<unknown>>;
