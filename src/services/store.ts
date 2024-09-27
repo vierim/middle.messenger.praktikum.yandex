7fa8b605fe73b02b97c8463b521df344c31c7f36
vierim <web77@yandex.ru>
2024-09-27 19:30:40 +0300

    fix: Add entities type to AppState

--- src/services/store.ts
+++ src/services/store.ts
@@ -2,12 +2,16 @@ import Store from '../core/store';
 
 import type { UserInfo } from '../entities/user';
 import type { ChatItem } from '../entities/chat';
+import type { Message } from '../entities/message';
 
 export type AppState = {
   isAuth: boolean;
   user: UserInfo | null;
   chats: ChatItem[];
   activeChat: ChatItem | null;
+  messages: Message[];
+  hasErrorEvent?: boolean;
+  errorMessage?: string;
 };
 
 const initialState: AppState = {
@@ -15,6 +19,9 @@ const initialState: AppState = {
   user: null,
   chats: [],
   activeChat: null,
+  messages: [],
+  hasErrorEvent: false,
+  errorMessage: '',
 };
 
 const store = new Store(initialState);
